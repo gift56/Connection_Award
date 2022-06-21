3960b0f57d830fa99a05124893ddb72e64cb8ea4
gift56 <efeasiughu@gmail.com>
2022-06-21 16:11:50 +0100

    started imporval of vote

--- styles/js/main.js
+++ styles/js/main.js
@@ -4,3 +4,12 @@ const showMenu = () => {
 }
 changeicon = (icon) => icon.classList.toggle('fa-times')
 Icons.addEventListener('click', showMenu);
+
+const openModal = () => {
+    voteM.classList.add('disp-f');
+}
+const closeModal = () => {
+    voteM.classList.remove('disp-f')
+}
+closeitem.addEventListener('click', closeModal);
+vote1.addEventListener('click', openModal);
